7e68c5066ebc1bfdd21e74f95027006ec93923fa
Joel Mueller <joel.mueller@gmail.com>
2021-12-29 15:21:50 -0600

    Isolating save benchmark

--- benchmarks/D2SLib_Local.Benchmark/LoadGame.cs
+++ benchmarks/D2SLib_Local.Benchmark/LoadGame.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using D2SLib;
+using D2SLib.Model.Save;
 
 namespace D2SLib_Benchmark;
 
@@ -7,6 +8,7 @@ namespace D2SLib_Benchmark;
 public class LoadGame
 {
     private byte[] _saveData = Array.Empty<byte>();
+    private D2S? _saveGame;
 
     [Benchmark]
     public void LoadOnly()
@@ -15,15 +17,15 @@ public class LoadGame
     }
 
     [Benchmark]
-    public void LoadAndSave()
+    public void SaveOnly()
     {
-        var saveGame = Core.ReadD2S(_saveData);
-        _ = Core.WriteD2S(saveGame);
+        _ = Core.WriteD2S(_saveGame!);
     }
 
     [GlobalSetup]
     public void GlobalSetup()
     {
         _saveData = File.ReadAllBytes(@"Resources\D2S\1.15\DannyIsGreat.d2s");
+        _saveGame = Core.ReadD2S(_saveData);
     }
 }
