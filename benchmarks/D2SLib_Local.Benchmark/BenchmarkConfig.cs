3a7c973832177138b62605f0d1a842750d382711
Joel Mueller <joel.mueller@gmail.com>
2021-12-28 19:54:23 -0600

    Benchmarks comparing to original

---
+++ benchmarks/D2SLib_Local.Benchmark/BenchmarkConfig.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+
+namespace D2SLib_Benchmark;
+
+internal class BenchmarkConfig : ManualConfig
+{
+    public BenchmarkConfig()
+    {
+        AddJob(Job.Default
+            .WithRuntime(CoreRuntime.Core60)
+            .WithPlatform(Platform.X64)
+            .WithJit(Jit.RyuJit));
+        AddJob(Job.Default
+            .WithRuntime(CoreRuntime.Core31)
+            .WithPlatform(Platform.X64)
+            .WithJit(Jit.RyuJit));
+        AddDiagnoser(MemoryDiagnoser.Default);
+        //AddExporter(CsvMeasurementsExporter.Default);
+        //AddExporter(HtmlExporter.Default);
+        AddExporter(MarkdownExporter.GitHub);
+        AddLogger(ConsoleLogger.Default);
+    }
+}
---
+++ benchmarks/D2SLib_Local.Benchmark/LoadGame.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using D2SLib;
+
+namespace D2SLib_Benchmark;
+
+[Config(typeof(BenchmarkConfig))]
+public class LoadGame
+{
+    private byte[] _saveData = Array.Empty<byte>();
+
+    [Benchmark]
+    public void LoadComplexSave()
+    {
+        _ = Core.ReadD2S(_saveData);
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _saveData = File.ReadAllBytes(@"Resources\D2S\1.15\DannyIsGreat.d2s");
+    }
+}
